5d0f19679e52dccceb60899b0a7c4674b9672c5c
Harsh Sangani <harshsangani001@gmail.com>
2025-01-29 00:58:38 +0530

    making backend using sanity

---
+++ backend/schemaTypes/workExperience.ts
@@ -0,0 +1,24 @@
+import { defineType } from 'sanity'
+
+export const workExperience = defineType({
+  name: 'workExperience',
+  title: 'WorkExperience',
+  type: 'document',
+  fields: [
+            {
+                name: 'name',
+               title:'name',
+               type:'string'
+            },
+            {
+                name:'company',
+                title:'Company',
+                type:'string'
+            },
+            {
+                name:'desc',
+                title:'Desc',
+                type:'string'
+            }
+  ],
+})
